8f912b4721cb84176bd426af24b0ac244030170e
Alex <AlexMarkGalvez@hotmail.com>
2020-10-25 17:20:16 -0400

    Initial commit

---
+++ src/components/ThePortfolio.vue
@@ -0,0 +1,29 @@
+<template>
+  <section id="two">
+    <h2>Recent Work</h2>
+    <div class="row">
+      <article class="col-6 col-12-xsmall work-item" v-for="(obj, key) in portfolioJSON" :key="key">
+        <a :href="obj.websiteLink" class="image fit thumb">
+            <img :src="'images/thumbs/' + obj.image" alt="">
+        </a>
+        <h3>{{ obj.title }}</h3>
+        <p>{{ obj.caption }}</p>
+      </article>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+    data: () => ({
+        portfolioJSON: [
+            {
+                image: 'djmichaeltoor.png',
+                title: 'DJ Michael Toor',
+                caption: 'As my first full stack project with node.js, this dj website allows the dj to advertise himself by presenting his music, bio, personal studio and his performed gigs to potential clients.',
+                websiteLink: 'https://djmichaeltoor.herokuapp.com/'
+            }
+        ]
+    })
+};
+</script>
